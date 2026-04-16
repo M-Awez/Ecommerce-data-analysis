6f2d1f35dfb354ee1ceb144799e38de3cbe551df
Awez <mdawez111@gmail.com>
2026-04-16 10:38:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+# Load model
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    day = int(request.form["day"])
+    
+    prediction = model.predict([[day]])
+    prediction = round(float(prediction), 2)
+
+    return render_template("index.html", result=prediction)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
